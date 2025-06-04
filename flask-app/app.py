872340628a6f954790c6f34675eeee6dfb37aaf4
Benny <132187364+Benny-17@users.noreply.github.com>
2025-06-05 02:37:01 +0530

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,10 +1,8 @@
-# app.py
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello from Benny's DevOps app!"
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
