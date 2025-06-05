39e67e007d019878d79a3fb96d9597ee5db07b00
Benny <132187364+Benny-17@users.noreply.github.com>
2025-06-05 20:01:24 +0530

    Update:v3app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Benny's ci/cd-demo!"
+    return "Hello from Benny's github actions-demo!"
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
